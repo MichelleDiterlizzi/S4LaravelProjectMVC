ef94aaebd08ff3cf77b0ce9999cf6066f7e64aba
Michelle Di Terlizzi <michelle.diterlizzi98@gmail.com>
2025-03-26 23:31:02 +0100

    index header started

---
+++ resources/views/index.blade.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    @vite(['resources/css/app.css'])
+    @vite(['resources/js/app.js'])
+    <title>Event Organizer</title>
+    
+</head>
+<body class="bg-[#eee3acc0] p-2 gap-2 flex flex-col items-center">
+    <header class="w-full bg-[#F9F5D2] p-4 flex h-20 justify-between items-center">
+        <h1 class="text-emerald-900 font-bold text-3xl font-serif w-[20%]">Event Organizer</h1>
+
+        <div class="flex w-[8%] justify-between items-center">
+            <svg class ='w-[30%]'xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
+                <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 6A2.25 2.25 0 0 1 6 3.75h2.25A2.25 2.25 0 0 1 10.5 6v2.25a2.25 2.25 0 0 1-2.25 2.25H6a2.25 2.25 0 0 1-2.25-2.25V6ZM3.75 15.75A2.25 2.25 0 0 1 6 13.5h2.25a2.25 2.25 0 0 1 2.25 2.25V18a2.25 2.25 0 0 1-2.25 2.25H6A2.25 2.25 0 0 1 3.75 18v-2.25ZM13.5 6a2.25 2.25 0 0 1 2.25-2.25H18A2.25 2.25 0 0 1 20.25 6v2.25A2.25 2.25 0 0 1 18 10.5h-2.25a2.25 2.25 0 0 1-2.25-2.25V6ZM13.5 15.75a2.25 2.25 0 0 1 2.25-2.25H18a2.25 2.25 0 0 1 2.25 2.25V18A2.25 2.25 0 0 1 18 20.25h-2.25A2.25 2.25 0 0 1 13.5 18v-2.25Z" />
+            </svg>
+            <a href="categorias">categorías</a>
+          </div>
+          <div class="relative w-full max-w-md mx-auto">
+            <div class="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none">
+                <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 text-gray-400" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" />
+                </svg>
+            </div>
+            
+            <input 
+                type="text" 
+                name="search" 
+                placeholder="Buscar..." 
+                class="w-full pl-10 pr-4 py-2 border border-gray-300 rounded-full text-gray-700 bg-white shadow-sm focus:outline-none focus:ring-0 focus:border-gray-500">
+        </div>
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6 mx-6">
+            <path stroke-linecap="round" stroke-linejoin="round" d="M15.75 6a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0ZM4.501 20.118a7.5 7.5 0 0 1 14.998 0A17.933 17.933 0 0 1 12 21.75c-2.676 0-5.216-.584-7.499-1.632Z" />
+          </svg>          
+    </header>
+    <div class="w-full h-96 bg-cover bg-center"  style="background-image: url('{{ asset('img/background3.jpg') }}')"></div>
+    <main></main>
+    <footer></footer>
+</body>
+</html>
