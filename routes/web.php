5a68da9beb325d2a90d7f887a7e988e832553d51
Michelle Di Terlizzi <michelle.diterlizzi98@gmail.com>
2025-04-08 10:14:02 +0200

    User folder created with edit and show.

--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,6 @@
 use App\Http\Controllers\EventController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
-use App\Http\Controllers\AuthController;
 use App\Http\Controllers\RegisterController;
 
 Route::get('/', function () {
