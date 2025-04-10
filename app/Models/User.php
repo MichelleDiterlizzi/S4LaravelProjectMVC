e0a9bfef374f4041da36d32e787a76d7950791b2
Michelle Di Terlizzi <michelle.diterlizzi98@gmail.com>
2025-04-10 13:32:45 +0200

    Method booted en User model added

--- app/Models/User.php
+++ app/Models/User.php
@@ -7,6 +7,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
+use Illuminate\Support\Facades\Log;
 
 class User extends Authenticatable
 {
@@ -41,4 +42,16 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+
+
+    protected static function booted(): void
+    {
+        static::deleted(function (User $user) {
+
+            Log::info("USUARIO BORRADO: ID={$user->id}, Email={$user->email}");
+
+        });
+
+    }
 }
